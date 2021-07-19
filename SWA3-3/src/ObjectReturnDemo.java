ca1f19527094594a14d53edda9efdfae9f0c5947
pobriley <59428856+pobriley@users.noreply.github.com>
2021-07-19 15:19:03 +0700

    Add files via upload

---
+++ SWA3-3/src/ObjectReturnDemo.java
@@ -0,0 +1,17 @@
+
+public class ObjectReturnDemo {
+	 int a;
+	  
+	    ObjectReturnDemo(int i)
+	    {
+	        a = i;
+	    }
+	  
+	    // This method returns an object
+	    ObjectReturnDemo incrByTen()
+	    {
+	        ObjectReturnDemo temp =
+	               new ObjectReturnDemo(a+10);
+	        return temp;
+        }
+}
